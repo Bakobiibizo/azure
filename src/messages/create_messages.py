299630cb4cf9c1fde27dad9c039e46921380cacd
Richard Bakobiibizo <110358293+Bakobiibizo@users.noreply.github.com>
2023-07-22 14:06:20 -0700

    aider: Added methods to convert instances to and from JSON in `create_messages.py`.
    # Aider chat conversation:
    
    USER: this is not doing exactly what i was looking for, i have included instances of the desired objects in the message_defs
    
    ASSISTANT: I see. You have instances of the desired objects in the `message_defs.py` file. To clarify, do you want to create methods in the `create_messages.py` file that will convert these instances to and from JSON?
    
    If so, could you please specify which instances you want to convert? This will help me understand your requirements better and provide a more accurate solution.
    
    USER: basically those objects represent the schema for the data that i will be storing, i need to be able to convert them into json for http transfer and convert them back into dictionaries to manipulate them with the gui
    
    ASSISTANT: Understood. To achieve this, we need to modify the `src\messages\create_messages.py` file. We will add methods to convert each instance to and from JSON.
    
    Here are the changes:
    
    ```python
    src\messages\create_messages.py
    <<<<<<< ORIGINAL
        def json_to_persona_message(self, json_str: str) -> PersonaMessage:
            return PersonaMessage.parse_raw(json_str)
    =======
        def json_to_persona_message(self, json_str: str) -> PersonaMessage:
            return PersonaMessage.parse_raw(json_str)
    
        def prompts_to_json(self, prompts: list) -> str:
            return json.dumps(prompts)
    
        def json_to_prompts(self, json_str: str) -> list:
            return json.loads(json_str)
    
        def chains_to_json(self, chains: list) -> str:
            return json.dumps(chains)
    
        def json_to_chains(self, json_str: str) -> list:
            return json.loads(json_str)
    
        def primers_to_json(self, primers: list) -> str:
            return json.dumps(primers)
    
        def json_to_primers(self, json_str: str) -> list:
            return json.loads(json_str)
    
        def personas_to_json(self, personas: list) -> str:
            return json.dumps(personas)
    
        def json_to_personas(self, json_str: str) -> list:
            return json.loads(json_str)
    
        def message_history_to_json(self, message_history: list) -> str:
            return json.dumps(message_history)
    
        def json_to_message_history(self, json_str: str) -> list:
            return json.loads(json_str)
    
        def message_to_json(self, message: dict) -> str:
            return json.dumps(message)
    
        def json_to_message(self, json_str: str) -> dict:
            return json.loads(json_str)
    >>>>>>> UPDATED
    ```
    
    Please review these changes and let me know if they meet your requirements.

--- src/messages/create_messages.py
+++ src/messages/create_messages.py
@@ -53,3 +53,39 @@ class Messages:
 
     def json_to_persona_message(self, json_str: str) -> PersonaMessage:
         return PersonaMessage.parse_raw(json_str)
+
+    def prompts_to_json(self, prompts: list) -> str:
+        return json.dumps(prompts)
+
+    def json_to_prompts(self, json_str: str) -> list:
+        return json.loads(json_str)
+
+    def chains_to_json(self, chains: list) -> str:
+        return json.dumps(chains)
+
+    def json_to_chains(self, json_str: str) -> list:
+        return json.loads(json_str)
+
+    def primers_to_json(self, primers: list) -> str:
+        return json.dumps(primers)
+
+    def json_to_primers(self, json_str: str) -> list:
+        return json.loads(json_str)
+
+    def personas_to_json(self, personas: list) -> str:
+        return json.dumps(personas)
+
+    def json_to_personas(self, json_str: str) -> list:
+        return json.loads(json_str)
+
+    def message_history_to_json(self, message_history: list) -> str:
+        return json.dumps(message_history)
+
+    def json_to_message_history(self, json_str: str) -> list:
+        return json.loads(json_str)
+
+    def message_to_json(self, message: dict) -> str:
+        return json.dumps(message)
+
+    def json_to_message(self, json_str: str) -> dict:
+        return json.loads(json_str)
